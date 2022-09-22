55cb7fc22428f42ce3a8fc94207e998528ce65ec
Caique Fernando Bezerra <caique@LAPTOP-UV2TD4UN.localdomain>
2022-09-22 15:00:17 -0300

    @caiqueb05 [Task: small corrections - 22.09.22]

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,29 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
-
 export interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/admin-page/dashboard', title: 'Dashboard', icon: 'nc-tile-56', class: '' },
-    { path: '/admin-page/create-teacher', title: 'Cadastrar Professor', icon: 'nc-hat-3', class: '' },
-    { path: '/admin-page/create-student', title: 'Cadastrar Estudante', icon: 'nc-circle-10', class: '' },
-    { path: '/admin-page/create-class', title: 'Cadastrar Aula', icon: 'nc-hat-3', class: '' },
+  {
+    path: "/admin-page/dashboard",
+    title: "Dashboard",
+    icon: "nc-tile-56",
+    class: "",
+  },
+  {
+    path: "/admin-page/create-student",
+    title: "Cadastrar Estudante",
+    icon: "nc-circle-10",
+    class: "",
+  },
+  {
+    path: "/admin-page/create-teacher",
+    title: "Cadastrar Professor",
+    icon: "nc-hat-3",
+    class: "",
+  },
+  {
+    path: "/admin-page/create-class",
+    title: "Cadastrar Aula",
+    icon: "nc-hat-3",
+    class: "",
+  },
 ];
 
 @Component({
-    moduleId: module.id,
-    selector: 'sidebar-cmp',
-    templateUrl: 'sidebar.component.html',
+  moduleId: module.id,
+  selector: "sidebar-cmp",
+  templateUrl: "sidebar.component.html",
 })
-
 export class SidebarComponent implements OnInit {
-    public menuItems: any[];
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
-    }
+  public menuItems: any[];
+  ngOnInit() {
+    this.menuItems = ROUTES.filter((menuItem) => menuItem);
+  }
 }
